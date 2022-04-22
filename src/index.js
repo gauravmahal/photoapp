7bc3433fe6dc2f0b2e8ece410399a7b255325d4f
Gaurav Mahal <gauravmahal3008@gmail.com>
2022-04-22 19:37:01 +0530

    Distributing application into react basic components covering section 4

--- src/index.js
+++ src/index.js
@@ -1,17 +1,66 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { Component } from 'react';
+// Renders our web page 
+// import ReactDom from 'react-dom';
+// React 18 
+import { createRoot } from 'react-dom/client';
+// if needed auto relading feature then will require Babel Watch or Node Mon 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// /////////////// Elements 
+// const tasks = ["take out trash", "shovel the driveway", "walk the dog"];
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// // it gets complicated when we try to scale that's why we use JSX in place of it 
+// const element = React.createElement("ol", null,
+//     tasks.map((task, index) => React.createElement('li', { key: index }, task))
+// );
+
+// // it is hardcoded list 
+// const element = <ol>
+//     <li>{tasks[0]}</li>
+//     <li>{tasks[1]}</li>
+//     <li>{tasks[2]}</li>
+// </ol>;
+
+// ////////////// JSX expression 
+// // div tag - is used for wrapping jsx element in enclosing tag
+// const element =
+//     <div>
+//         <h1> Task List</h1>
+//         <ol>
+//             {tasks.map((task, index) => <li key={index}>{task}</li>)}
+//         </ol>
+//     </div>
+
+// ////////// Components 
+// // ES6 class used for declaring components 
+// // render should return react elements on what should appers on screen 
+// class List extends Component {
+//     render() {
+//         return (<ol>
+//             {this.props.tasks.map((task, index) => <li key={index}>{task}</li>)}
+//         </ol>)
+//     }
+// }
+
+// class Title extends Component {
+//     render() {
+//         return <h1> {this.props.title}</h1>
+//     }
+// }
+
+// class Main extends Component {
+//     render() {
+//         return <div>
+//             <Title title={"To Do's"} />
+//             <List tasks={['Mown the lawn', 'walk the dog']} />
+//             <List tasks={['house the driveway', 'finish the laundry']} />
+//         </div>
+//     }
+// }
+
+//////////// Refactoring 
+import Main from './Components/Main';
+
+// // Deprecated in react 18 
+// ReactDom.render(element, document.getElementById('root'))
+const root = createRoot(document.getElementById('root'));
+root.render(<Main />)
