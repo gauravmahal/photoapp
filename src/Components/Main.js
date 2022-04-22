c0ee30cdb60e4eff01038f1f6a97f2b0bd5147ad
Gaurav Mahal <gauravmahal3008@gmail.com>
2022-04-22 19:37:01 +0530

    Class component architecture photo App is formed

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,15 +1,31 @@
 import React, { Component } from "react";
-import List from './List';
 import Title from './Title';
+import Photowall from './PhotoWall';
+const posts = [{
+    id: "0",
+    description: "beautiful landscape",
+    imageLink: "https://image.jimcdn.com/app/cms/image/transf/none/path/sa6549607c78f5c11/image/i4eeacaa2dbf12d6d/version/1490299332/most-beautiful-landscapes-in-europe-lofoten-european-best-destinations-copyright-iakov-kalinin.jpg" +
+        "3919321_1443393332_n.jpg"
+}, {
+    id: "1",
+    description: "Aliens???",
+    imageLink: "https://www.science.org/do/10.1126/science.aax3525/full/Iridium52_16x9.jpg"
+}, {
+    id: "2",
+    description: "On a vacation!",
+    imageLink: "https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2017/08/24/104670887-VacationExplainsTHUMBWEB.1910x1000.jpg"
+}]
 
 class Main extends Component {
     render() {
         return <div>
-            <Title title={"To Do's"} />
-            <List tasks={['Mown the lawn', 'walk the dog']} />
-            <List tasks={['house the driveway', 'finish the laundry']} />
+            <Title title={"Photowall"} />
+            <Photowall posts={posts} />
         </div>
     }
 }
 
 export default Main
+
+//https://www.science.org/do/10.1126/science.aax3525/full/Iridium52_16x9.jpg
+//https://img.wallpapersafari.com/desktop/1600/900/45/75/oC157f.jpg
--- src/index.js
+++ src/index.js
@@ -1,64 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 // Renders our web page 
 // import ReactDom from 'react-dom';
 // React 18 
 import { createRoot } from 'react-dom/client';
-// if needed auto relading feature then will require Babel Watch or Node Mon 
-
-// /////////////// Elements 
-// const tasks = ["take out trash", "shovel the driveway", "walk the dog"];
-
-// // it gets complicated when we try to scale that's why we use JSX in place of it 
-// const element = React.createElement("ol", null,
-//     tasks.map((task, index) => React.createElement('li', { key: index }, task))
-// );
-
-// // it is hardcoded list 
-// const element = <ol>
-//     <li>{tasks[0]}</li>
-//     <li>{tasks[1]}</li>
-//     <li>{tasks[2]}</li>
-// </ol>;
-
-// ////////////// JSX expression 
-// // div tag - is used for wrapping jsx element in enclosing tag
-// const element =
-//     <div>
-//         <h1> Task List</h1>
-//         <ol>
-//             {tasks.map((task, index) => <li key={index}>{task}</li>)}
-//         </ol>
-//     </div>
-
-// ////////// Components 
-// // ES6 class used for declaring components 
-// // render should return react elements on what should appers on screen 
-// class List extends Component {
-//     render() {
-//         return (<ol>
-//             {this.props.tasks.map((task, index) => <li key={index}>{task}</li>)}
-//         </ol>)
-//     }
-// }
-
-// class Title extends Component {
-//     render() {
-//         return <h1> {this.props.title}</h1>
-//     }
-// }
-
-// class Main extends Component {
-//     render() {
-//         return <div>
-//             <Title title={"To Do's"} />
-//             <List tasks={['Mown the lawn', 'walk the dog']} />
-//             <List tasks={['house the driveway', 'finish the laundry']} />
-//         </div>
-//     }
-// }
-
 //////////// Refactoring 
 import Main from './Components/Main';
+
+import './styles/stylesheets.css';
 
 // // Deprecated in react 18 
 // ReactDom.render(element, document.getElementById('root'))
