f931ba0c4ac94839236aa27e69eda42ba5637c60
Gaurav Mahal <gauravmahal3008@gmail.com>
2022-04-24 21:31:43 +0530

    Imcorporating lifecycle methods

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -7,22 +7,10 @@ class Main extends Component {
     constructor() {
         super()
         this.state = {
-            posts: [{
-                id: "0",
-                description: "beautiful landscape",
-                imageLink: "https://image.jimcdn.com/app/cms/image/transf/none/path/sa6549607c78f5c11/image/i4eeacaa2dbf12d6d/version/1490299332/most-beautiful-landscapes-in-europe-lofoten-european-best-destinations-copyright-iakov-kalinin.jpg" +
-                    "3919321_1443393332_n.jpg"
-            }, {
-                id: "1",
-                description: "Aliens???",
-                imageLink: "https://www.science.org/do/10.1126/science.aax3525/full/Iridium52_16x9.jpg"
-            }, {
-                id: "2",
-                description: "On a vacation!",
-                imageLink: "https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2017/08/24/104670887-VacationExplainsTHUMBWEB.1910x1000.jpg"
-            }]
+            posts: []
         }
         this.removePhoto = this.removePhoto.bind(this)
+        console.log('constructor')
     }
 
     removePhoto(postRemoved) {
@@ -31,13 +19,45 @@ class Main extends Component {
             posts: state.posts.filter(post => post !== postRemoved)
         }))
     }
+    // called after components is inserted into the Dom 
+    componentDidMount() {
+        const data = SimulateFetchFromDatabase();
+        this.setState({
+            posts: data
+        })
+        console.log('componentDidMount')
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        // alert('re-render')
+        console.log(prevState.posts)
+        console.log(this.state)
+    }
 
     render() {
+        console.log('render')
         return <div>
             <Title title={"Photowall"} />
             <PhotoWall posts={this.state.posts} onRemovePhoto={this.removePhoto} />
         </div>
     }
+}
+
+function SimulateFetchFromDatabase() {
+    return [{
+        id: "0",
+        description: "beautiful landscape",
+        imageLink: "https://image.jimcdn.com/app/cms/image/transf/none/path/sa6549607c78f5c11/image/i4eeacaa2dbf12d6d/version/1490299332/most-beautiful-landscapes-in-europe-lofoten-european-best-destinations-copyright-iakov-kalinin.jpg" +
+            "3919321_1443393332_n.jpg"
+    }, {
+        id: "1",
+        description: "Aliens???",
+        imageLink: "https://www.science.org/do/10.1126/science.aax3525/full/Iridium52_16x9.jpg"
+    }, {
+        id: "2",
+        description: "On a vacation!",
+        imageLink: "https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2017/08/24/104670887-VacationExplainsTHUMBWEB.1910x1000.jpg"
+    }]
 }
 
 export default Main
